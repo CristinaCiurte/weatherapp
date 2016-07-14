39d9297073e2aedebb52f7565c2d056b9d66f2b6
info <info@ebs.msft>
2016-07-14 11:48:55 +0300

    Add project

--- javascript/components/Client.js
+++ javascript/components/Client.js
@@ -3,14 +3,24 @@ import axios from 'axios'
 const Client = () => {
 
   var
-    base_url = 'http://api.openweathermap.org/data/2.5/weather/',
+    base_url = 'http://api.openweathermap.org/data/2.5/',
     api_key = 'b08540e9336bc45708303709709c330f'
+
+
 
   async function getCurrentWeather(location) {
     return await call(
-      auth(base_url + '?q=' + location)
+      auth(base_url + 'weather?q=' + location)
     )
   }
+  
+  async function getForecastWeather(location) {
+    return await call(
+        auth(base_url + 'forecast/daily?q=' + location + '&cnt=7')
+    )
+    
+  }
+
 
   function auth(url) {
     return url + '&APPID=' + api_key
@@ -21,8 +31,11 @@ const Client = () => {
   }
 
   return {
-    getCurrentWeather: getCurrentWeather
+    getCurrentWeather: getCurrentWeather,
+    getForecastWeather: getForecastWeather
   }
+
+ 
 }
 
 export default Client;
--- javascript/main.js
+++ javascript/main.js
@@ -2,8 +2,92 @@ import Client from './components/Client'
 
 var WeatherClient = new Client();
 
-WeatherClient.getCurrentWeather('Sibiu').then(function(response){
-  console.log(response)
-}).catch(function(err) {
-  console.warn(err)
-})
+
+
+function Location(e) {
+
+  if(e.type == 'keyup' && e.keyCode != 13){
+    return false;
+  }
+
+  WeatherClient.getCurrentWeather(document.getElementById('city').value).then(function (response) {
+
+    if(document.getElementById('city').value ==0){
+      alert("Insert the name of the city!");
+    }
+
+    var html = '';
+
+  
+    var celsiusDegrees;
+    celsiusDegrees=(response.data.main.temp - 273.15).toPrecision(2);
+    
+
+    html += '\n' + document.getElementById('city').value + '\n';
+    html += "\n \n Cloudiness: " +  response.data.weather[0].description + '.\n' ;
+    html += "Temperature: " + celsiusDegrees +  ' ℃. \n';
+    html += "Humidity: " + response.data.main.humidity  + '%. \n';
+    html += "Wind speed: " + response.data.wind.speed + ' mph. \n';
+
+    document.getElementById('image').setAttribute('src', 'img/' + response.data.weather[0].icon + '.svg') ;
+    document.getElementById("vreme").innerText = html;
+
+
+
+    //document.getElementById('search_input').classList.toggle('hidden');
+    document.getElementById('result_container').classList.remove('hidden');
+    document.getElementById('result_container2').classList.add('hidden');
+
+
+    console.log(response.data)
+
+  }).catch(function (err) {
+    console.warn(err)
+  })
+}
+  function Forecast(e){
+    WeatherClient.getForecastWeather(document.getElementById('city').value).then(function (response) {
+      console.log(response);
+
+      var html= '';
+      var i;
+      var a = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+      var date = new Date();
+      var day = date.getDay();
+
+      a = a.concat(a.splice(0, day))
+      for(i = 0; i<=6; i++){
+
+
+
+        html += '<br/> The weather on ' +  a[i];
+        html += "<br/>  Cloudiness: " +  response.data.list[i].weather[0].description + '.<br/>' ;
+        html += "Temperature: " + (response.data.list[i].temp.day - 273.15).toPrecision(2) +  ' ℃. <br/>';
+        html += "Humidity: " + response.data.list[i].humidity  + '%. <br/>';
+        html += "Wind speed: " + response.data.list[i].speed + ' mph. <br/>';
+        html += '<img src="img/' + response.data.list[i].weather[0].icon + '.svg" />'+  '<br>';
+
+      }
+
+      
+      document.getElementById('vremea').innerHTML = html;
+
+      document.getElementById('result_container2').classList.remove('hidden');
+      
+
+    }).catch(function (err) {
+      console.warn(err)
+    })
+    
+  }
+
+
+document.getElementById("myBtn").addEventListener("click", Location);
+document.getElementById('city').addEventListener('keyup', Location);
+document.writeln('');
+document.getElementById('current_date').innerHTML = new Date();
+document.writeln('');
+
+document.getElementById("myBtn2").addEventListener("click", Forecast);
+
+
